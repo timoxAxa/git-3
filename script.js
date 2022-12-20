2630b5eef3f7292a4791008bcea277dc587cf89d
timoxAxa <kushmamatovtimur@gmail.com>
2022-12-20 07:08:42 +0500

    бугатти < самокат

---
+++ script.js
@@ -0,0 +1,8 @@
+let names = "Мухлиса Илон Дарья Девид Ислом Шах Тимоха Равшан Ибрагим Далер Далер Александр Анатолий"
+let n = /[А-яЁё]+/g[Symbol.match](names).sort((a,b)=>b.length-a.length).filter(i=> x==0 ? x=i.length : i.length==x, x=0)
+console.log(n);
+
+let cars = [10000, 16000, 25000, 35000, 50000,11000, 16000, 25000, 35000, 50000,70000, 15000]
+
+let cheap = [/[0-9]+/g[Symbol.match](cars).sort((a,b)=>a-b).filter(i=> x==0 ? x=i.length : i.length==x, x=0)]
+console.log(cheap);
